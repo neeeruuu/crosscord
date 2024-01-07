160588837d091e5b00b0c3e302fdbc807d4c501a
neeeruuu <152752583+neeeruuu@users.noreply.github.com>
2024-01-07 13:50:53 -0300

    delete copy operator

--- src/crosscord/util/macros.h
+++ src/crosscord/util/macros.h
@@ -3,6 +3,7 @@
 #define DECLARE_SINGLETON(ClassName) private: \
 	static ClassName* _Instance;\
 public:\
+	ClassName(const ClassName&) = delete;\
 	static ClassName* Get() {\
 		if (!_Instance)\
 			_Instance = new ClassName();\
