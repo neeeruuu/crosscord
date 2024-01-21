b2d04bafe2cc65b0e8b222988076cf8dd8154588
neeeruuu <152752583+neeeruuu@users.noreply.github.com>
2024-01-21 01:24:42 -0300

    fix: change the way singletons are instantiated

--- src/crosscord/util/macros.h
+++ src/crosscord/util/macros.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#define DECLARE_SINGLETON(ClassName) private: \
-	static ClassName* _Instance;\
-public:\
-	ClassName(const ClassName&) = delete;\
-	static ClassName* Get() {\
-		if (!_Instance)\
-			_Instance = new ClassName();\
-		return _Instance;\
-	}
 
-#define INITIALIZE_SINGLETON(ClassName) ClassName* ClassName::_Instance = nullptr
+#define DECLARE_SINGLETON(ClassName) private: \
+	static ClassName* _Instance; \
+public: \
+	ClassName(const ClassName&) = delete; \
+	static ClassName* Get() { return _Instance; }
+
+#define INSTANTIATE_SINGLETON(ClassName) ClassName* ClassName::_Instance = new ClassName();
