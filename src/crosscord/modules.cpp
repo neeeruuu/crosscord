a5703b3700f8e4088377bd628fd604acaf27071d
neeeruuu <152752583+neeeruuu@users.noreply.github.com>
2024-01-20 21:07:05 -0300

    feat: wait for all threads to finish instead of just one

--- src/crosscord/modules.cpp
+++ src/crosscord/modules.cpp
@@ -75,18 +75,70 @@ void CModuleManager::AwaitFutures() {
 	std::chrono::milliseconds FutureInterval(FUTURE_WAIT_INTERVAL);
 	bool bThreadHalted = false;
 	
+	size_t iTotalFutureCount = m_Futures.size();
+
+	/*
+		wait for at least one thread to stop
+	*/
 	while (!bThreadHalted && m_Initialized) {
-		for (std::future<bool>& Future : m_Futures) {
-			std::future_status Status = Future.wait_for(FutureInterval);
+		for (auto Future = m_Futures.rbegin(); Future != m_Futures.rend();) {
+			std::future_status Status = Future->wait_for(FutureInterval);
 
-			if (Status != std::future_status::ready)
+			if (Status != std::future_status::ready) {
+				++Future;
 				continue;
+			}
 
-			if (!Future.get())
+			if (!Future->get())
 				LogWarning("Module futured stopped with error");
+
+			Future = std::vector<std::future<bool>>::reverse_iterator(m_Futures.erase(--Future.base()));
 
 			bThreadHalted = true;
 			break;
 		}
 	}
+
+	/*
+		wait for other threads to stop or timeout
+	*/
+	auto StartTime = std::chrono::system_clock::now().time_since_epoch();
+
+	size_t iFutureCount = m_Futures.size();
+	bool bTimedOut = false;
+	while (iFutureCount > 0 || bTimedOut) {
+		/*
+			remove futures as they finished
+		*/
+		for (auto Future = m_Futures.rbegin(); Future != m_Futures.rend();) {
+			std::future_status Status = Future->wait_for(FutureInterval);
+
+			if (Status != std::future_status::ready) {
+				++Future;
+				continue;
+			}
+
+			if (!Future->get())
+				LogWarning("Module futured stopped with error");
+
+			Future = std::vector<std::future<bool>>::reverse_iterator(m_Futures.erase(--Future.base()));
+		}
+
+		/*
+			check if time elapsed is greater than timeout
+		*/
+		auto CurrTime = std::chrono::system_clock::now().time_since_epoch();
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(CurrTime - StartTime).count() > FUTURE_TIMEOUT) {
+			bTimedOut = true;
+			break;
+		}
+
+		iFutureCount = m_Futures.size();
+		std::this_thread::sleep_for(FutureInterval);
+	}
+
+	if (!bTimedOut)
+		LogInfo("All futures stopped");
+	else
+		LogWarning("Not all of the futures stopped in time ({}/{})", m_Futures.size(), iTotalFutureCount);
 }
